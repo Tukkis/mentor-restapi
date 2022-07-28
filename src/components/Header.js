da78b0228ed6cfd534ae38fbaece86d3c77f4aa2
Tukkis <54272662+Tukkis@users.noreply.github.com>
2022-07-28 13:39:50 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({...modeChangeHandler}) {
     return (
       <div className="flex p-10 py-7 shadow-lg">
         <div className="grow font-NunitoBlack text-2xl">Where in the world?</div>
-        <div className="font-NunitoLight">☾ Dark mode</div>
+        <div className="font-NunitoLight">☾ dDark mode</div>
       </div>
     );
   }
